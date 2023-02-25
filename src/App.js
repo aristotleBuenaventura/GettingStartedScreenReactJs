b43e766a0f02234c99a49d8e914c9d817019aa66
aristoleBuenaventura <aristotlecortezbuenaventura@gmail.com>
2023-02-25 12:56:56 +0800

    initial files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import aris from './aris.jpg'
+import { ArrowRight } from 'react-bootstrap-icons';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container text-center'>
+        <p className='h1 mt-5 mb-3'>Who is Aristotle?</p>
+        <img src={aris} style={{ height: '350px' }} />
+        <p className='h5 mt-4'>Aristotle was a Greek philosopher and polymath who made important contributions to fields such as logic, biology, and ethics.</p>
+        <p className='font-bold mt-4'>By Buenaventura, Aristotle C.</p>
+        <button type="button" class="btn btn-primary btn-lg mt-5 rounded rounded-pill">Know more about him  <ArrowRight color="white" size={28} /></button>
+      </div>
     </div>
   );
 }
